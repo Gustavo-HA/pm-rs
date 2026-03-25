39d45921638edf0159166bca060e137623abb233
Gustavo Hdz <gustavoha40@gmail.com>
2026-03-25 14:00:21 -0600

    refactor(run_models.sh): update SLURM job configuration and improve script structure

--- scripts/run_models.sh
+++ scripts/run_models.sh
@@ -1,40 +1,30 @@
 #!/bin/bash
-
-#SBATCH --partition=GPU
-#SBATCH --job-name=RecSysRunModels
-#SBATCH --nodes=1
+#SBATCH --partition=C0
+#SBATCH --job-name=recsys_run_models
+#SBATCH --output=scripts/run_models.log
+#SBATCH --error=scripts/run_models.err
 #SBATCH --ntasks=1
-#SBATCH --output="./run_models.log"
-#SBATCH --error="./run_models.err"
+#SBATCH --cpus-per-task=32
+#SBATCH --nodes=1
 #SBATCH --mem=0
-#SBATCH --time=0
+#SBATCH --time=03:00:00
 
 # Enviar correo electrónico cuando el trabajo finalice o falle
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=gustavo.angeles@cimat.mx
 
-####################### CONFIGURACION DE UV #######################
-
 # add uv to path
 export PATH="/home/est_posgrado_gustavo.angeles/.local/uv/bin:$PATH"
 
+set -euo pipefail
 
-# change to the directory where the job was submitted
-cd "/home/est_posgrado_gustavo.angeles/Proyectos/pm-rs/"
+cd "$SLURM_SUBMIT_DIR"
 
-####################### EJECUCION DEL SCRIPT DE PYTHON #######################
+echo "=== Inicio: $(date) ==="
 
-PYTHON_SCRIPT="./scripts/run_models.py"
+echo "Nodo: $SLURMD_NODENAME"
+echo "CPUs asignados: $SLURM_CPUS_PER_TASK"
 
-echo "Ejecutando el script de Python: $PYTHON_SCRIPT"
+uv run python scripts/run_models.py --k 5 10 20 --output scripts/run_models_results.csv
 
-# Usar 'uv run' para asegurar que se usen las dependencias del proyecto
-# Usar torchrun para DDP
-uv run "$PYTHON_SCRIPT" --k 5 10 20 --output "./scripts/results_run_models.csv"
-
-if [ $? -ne 0 ]; then
-    echo "ERROR: El script de Python falló."
-    exit 1
-fi
-
-echo "--- Trabajo de Slurm Finalizado ---"
+echo "=== Fin: $(date) ==="
